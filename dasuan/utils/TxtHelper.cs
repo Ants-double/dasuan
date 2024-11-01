442bb405f23684f25863d6340c4ae6daf151b9a1
liyangyang <ants_double@yeah.net>
2024-11-01 15:44:33 +0800

    txt write“

---
+++ dasuan/utils/TxtHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dasuan.utils
+{
+    public class TxtHelper
+    {
+
+        #region write
+        /** 
+         * 
+         **/
+        public void Write(string path,string content)
+        {
+            FileStream fs = new FileStream(path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            //开始写入
+            sw.Write(content);
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+        }
+        public void WriteFile(string path, string content)
+        {
+            FileStream fs = new FileStream(path, FileMode.Create);
+            //获得字节数组
+            byte[] data = System.Text.Encoding.Default.GetBytes(content);
+            //开始写入
+            fs.Write(data, 0, data.Length);
+            //清空缓冲区、关闭流
+            fs.Flush();
+            fs.Close();
+        }
+        public void WriteList<T>(string path, List<T> collection)
+        {
+            FileStream fs = new FileStream(path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            //开始写入
+            foreach (var item in collection)
+            {
+                sw.WriteLine(item);
+            }
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+        }
+        #endregion
+
+
+    }
+}
